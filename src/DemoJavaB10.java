625a45e927129e7d28284c9e610e7dd20a0dba78
YalDarraji <140679762+YalDarraji@users.noreply.github.com>
2023-07-29 11:45:58 -0400

    Update DemoJavaB10.java

--- src/DemoJavaB10.java
+++ src/DemoJavaB10.java
@@ -3,5 +3,8 @@ public class DemoJavaB10 {
 	public static void main(String[] args) {
 		System.out.println("B10 Class 2023");
 	}
+	public static void subtract ( int a , int b){
+		System.out.println( a - b );
+	}
 
 }
